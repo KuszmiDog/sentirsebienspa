62fb6b55a82fad7ed1b51873e69caf9d7aef502a
KuszmiDog <germankuszmirukvispo@gmail.com>
2025-05-27 12:55:54 -0300

    tratando de conectar el back

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path'); // Importa path para manejar rutas
 
 const app = express();
-const PORT = 12571;
+const PORT = 3000;
 
 // Habilitar CORS
 app.use(cors());
